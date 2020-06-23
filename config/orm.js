24d994a28d3868e2964a5bed6569d7218f91dad3
Bryce Speirs <baspeirs@gmail.com>
2020-06-23 11:12:43 -0400

    connected server to orm to database

---
+++ config/orm.js
@@ -0,0 +1,3 @@
+// require our connection file
+const connection = require("./connection");
+
--- server.js
+++ server.js
@@ -18,9 +18,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require(""); // ===== use this to require controller files // make sure to get the path right ======
+//const routes = require(""); // ===== use this to require controller files // make sure to get the path right ======
 // use the controller files
-app.use(routes);
+//app.use(routes);
+
+// require our orm file ---- orm will do the connection to the database
+const orm = require("./config/orm")
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
