7e13f148d4e6315dbbd9af022d045ee4b8868d05
Bryce Speirs <baspeirs@gmail.com>
2020-06-23 11:00:11 -0400

    set up database with id as primary key to auto increment, burger name to take in strings, and devoured to take in a boolean

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cowboy Bacon", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Frisco", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Fried Macaroni Burger", false);
