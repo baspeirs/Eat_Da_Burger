b33b62a7498eda231ea2dcd001f76a1ac1f4c9d0
Bryce Speirs <baspeirs@gmail.com>
2020-06-24 22:29:34 -0400

    set up model file

---
+++ models/burgModel.js
@@ -0,0 +1,18 @@
+const orm = require("../config/orm");
+
+
+const cat = {
+    selectAll: (cb) => {
+        orm.selectAll("burgers", (res) => cb(res))
+    },
+
+    insertOne: (col1, col2, val1, val2, cb) => {
+        orm.insertOne("burgers", col1, col2, val1, val2,  (res) => cb(res))
+    },
+
+    updateOne: (col1, val1, col2, val2, id, cb) => {
+        orm.updateOne("burgers", col1, val1, col2, val2, id, (res) => cb(res))
+    }
+}
+
+module.exports = cat;
