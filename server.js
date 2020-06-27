9d39c98c8e832c89ec0623804345168a7fc32c53
Bryce Speirs <baspeirs@gmail.com>
2020-06-27 01:05:08 -0400

    another attempt

--- server.js
+++ server.js
@@ -17,14 +17,10 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(express.static("public"))
-
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "views/layouts/main.handlebars"));
-});
+app.use(express.static("public"));
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/burgControl"); // ===== use this to require controller files // make sure to get the path right ======
+const routes = require("./controllers/burgControl"); 
 // use the controller files
 app.use(routes);
 
