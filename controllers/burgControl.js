9468d1087d4545deb02049692a8d84c0e8c80422
Bryce Speirs <baspeirs@gmail.com>
2020-06-26 20:37:01 -0400

    sucessfully able to add new burger and reload page hosting new burger

--- controllers/burgControl.js
+++ controllers/burgControl.js
@@ -8,9 +8,16 @@ router.get("/", function(req, res) {
       var hbsObject = {
         burgers: data
       };
-      console.log(hbsObject);
       res.render("index", hbsObject);
     });
   });
+
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], result => {
+    // Send back the ID of the new burger
+    res.json({ id: result.insertId });
+    //==== DO NOT CALL THIS TWICE!!! res.json({ id: result.insertId }) ========
+  });
+});
 
   module.exports = router;
--- models/burgModel.js
+++ models/burgModel.js
@@ -7,7 +7,7 @@ var burger = {
     },
 
     insertOne: (col1, col2, val1, val2, cb) => {
-        orm.insertOne("burgers", col1, col2, val1, val2,  (res) => cb(res))
+        orm.insertOne("burgers", col1, col2, val1, val2, (res) => cb(res))
     },
 
     updateOne: (col1, val1, col2, val2, id, cb) => {
